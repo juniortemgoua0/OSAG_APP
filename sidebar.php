2c44311508bc35807f38f8af6f10799591cfdb80
juniortemgoua0 <juniortemgoua0@gmail.com>
2022-03-19 13:28:35 +0100

    mend

---
+++ sidebar.php
@@ -0,0 +1,65 @@
+<div class="sidebar">
+  <div class="logo-details">
+    <span class="logo_name">OSAG</span>
+    <div class="logo_desc">osag service</div>
+  </div>
+  <div class=" menu-up">
+    <i class="bi bi-menu-up text-white fs-2 mb-5 mt-4"></i>
+  </div>
+  <ul class="nav-links">
+    <li>
+      <a href="#" class="active">
+        <i class='bx bx-grid-alt'></i>
+        <span class="links_name">Dashboard</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-box'></i>
+        <span class="links_name">Products</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-pie-chart-alt-2'></i>
+        <span class="links_name">Analytics</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-coin-stack'></i>
+        <span class="links_name">Stocks</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-book-alt'></i>
+        <span class="links_name">Total order</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-user'></i>
+        <span class="links_name">Team</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-message'></i>
+        <span class="links_name">Messages</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-cog'></i>
+        <span class="links_name">Setting</span>
+      </a>
+    </li>
+    <li class="log_out">
+      <a href="#">
+        <i class='bx bx-log-out'></i>
+        <span class="links_name">Log out</span>
+      </a>
+    </li>
+  </ul>
+</div>
